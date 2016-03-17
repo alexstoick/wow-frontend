29c6cb8154daa497aefd18822d4ebfe384ade6c4
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2016-03-17 11:48:52 +0000

    items.js

--- app/components/items.js
+++ app/components/items.js
@@ -7,34 +7,31 @@ export default class Items extends React.Component {
   constructor(props) {
     super(props)
     this.state.itemIds = [110656, 113264]
+    this.state.elements = []
   }
 
   async componentDidMount() {
-    itemId = this.props.itemId || this.props.params.itemId
-    const result = await fetch('http://82.196.11.214:3000/v1/items/' + this.props.params.itemId + '/crafts')
-    const json = await result.json();
-    this.setState({summary: json});
+    this.state.itemIds.map(
+      async function(itemId, i){
+        const result = await fetch('http://82.196.11.214:3000/v1/items/' + itemId)
+        const json = await result.json();
+        let newElements = this.state.elements + json
+        this.setState({elements: newElements});
+      }
+    )
     console.log(json);
   }
 
   render() {
-    if (this.state.summary) {
+    if (this.state.elements) {
       return (
         <div>
-          {this.state.summary.map(
-            function(craft, i) {
+          {this.state.elements.map(
+            function(element, i) {
+              console.log(element);
               return (
                 <div>
-                  Name: {craft.SpellName}<br/>
-                  Profession: {craft.Profession}
-                  <br/> <br/>
-                  {craft.Items.map(
-                      function(item,i) {
-                        return <CraftItem
-                          item={item}
-                        />
-                      })
-                  }
+                  <ItemSummary item={element}/>
                 </div>
               )
             })
--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ItemSummary from './components/item_summary.js'
 import CraftSummary from './components/craft_summary.js'
+import Items from './components/items.js'
 import { Router, Route, browserHistory } from 'react-router'
 
 ReactDOM.render(
