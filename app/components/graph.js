2668da118fab0e0e8f7cba59d999c5bf94797405
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2016-03-23 15:11:28 +0200

    added fetch

--- app/components/graph.js
+++ app/components/graph.js
@@ -1,38 +1,54 @@
-import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries, VerticalBarSeries} from 'react-vis';
+import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries, VerticalBarSeries, VerticalGridLines} from 'react-vis';
 import React from 'react';
+import fetch from 'isomorphic-fetch';
 
 export default class Graph extends React.Component {
-  render() {
-    return (
-      //<XYPlot
-        //width={300}
-        //height={300}>
-        //<HorizontalGridLines />
-        //<XAxis title="X Axis" />
-        //<YAxis title="Y Axis" />
-        //<LineSeries
-          //data={[
-            //{x: 1, y: 3},
-            //{x: 2, y: 5},
-            //{x: 3, y: 15},
-            //{x: 4, y: 12}
-          //]}/>
-        //<LineSeries
-          //data={[
-            //{x: 1, y: 10},
-            //{x: 2, y: 4},
-            //{x: 3, y: 2},
-            //{x: 4, y: 15}
-          //]}/>
-      //</XYPlot>
-        <XYPlot width={300} height={300}>
-          <XAxis />
-          <YAxis />
-          <HorizontalGridLines />
-          <LineSeries data={[{x: 1, y: 10}, {x: 2, y: 5}, {x: 3, y: 15}]} />
-          <VerticalBarSeries data={[{x: 1, y: 11}, {x: 2, y: 7}, {x: 3, y: 20}]} />
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.state.lineChartData = []
+    this.state.barChartData = []
+  }
 
-        </XYPlot>
-    );
+
+  async componentDidMount() {
+    const result = await fetch('http://82.196.11.214:3000/v1/items/118472/history_price');
+    const json = await result.json();
+
+    json.map((data_point,i) => {
+      if(i > 2) {
+        return
+      }
+      let newLineChartPoint = this.state.lineChartData;
+      newLineChartPoint.push({ x:i, y: data_point["Average"]/10000});
+
+      let newBarCharPoint = this.state.barChartData;
+      newBarCharPoint.push({ x:i, y: data_point["Count"]/10000});
+
+      this.setState({lineChartData: newLineChartPoint,barChartData: newBarCharPoint});
+    });
+  }
+
+  render() {
+    if(this.state.barCharData) {
+      return (
+          "LOADING"
+      );
+    } else {
+      console.log('rendering');
+      console.log(this.state.lineChartData);
+      let data = [ {x:0, y:10.73219}, {x:2, y:15}]
+      console.log(data);
+      return (
+          <XYPlot width={1000} height={300}>
+            <XAxis />
+            <YAxis />
+            <HorizontalGridLines />
+            <VerticalGridLines />
+            <LineSeries data={this.state.lineChartData} />
+
+          </XYPlot>
+      );
+    }
   }
 }
